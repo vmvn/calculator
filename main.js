b07f23ddabfdce27de8dfc87ffc4abf1781da882
vmvn <54136060+vmvn@users.noreply.github.com>
2025-08-20 12:45:59 +0300

    Add helper functions

---
+++ main.js
@@ -0,0 +1,29 @@
+let number1, number2, operator;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, n1, n2) {
+  if (operator == 'addition') {
+    return add(n1, n2);
+  } else if (operator == 'subtraction') {
+    return subtract(n1, n2);
+  } else if (operator == 'multiplication') {
+    return multiply(n1, n2);
+  } else if (operator == 'division') {
+    return divide(n1, n2);
+  }
+}
